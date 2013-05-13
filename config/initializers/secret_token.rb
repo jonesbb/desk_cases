0a607e272eea501b33ad85626ec930c9f91cf69b
Byron Jones <jonesbb@gmail.com>
2013-05-12 21:27:42 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeskCases::Application.config.secret_token = '902af3447491f5719170c8e47ad5e76b01078910287207dfb58609c0211cb5a465bd88479584254bec6e4f896447648ff456c4f576009d601f420b1e795d4150'
